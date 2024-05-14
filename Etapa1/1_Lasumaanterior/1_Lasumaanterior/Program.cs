b201a7a6d4f705833bdb0b473294193038c0371b
AcramJ <167466680+AcramJ@users.noreply.github.com>
2024-05-13 21:56:35 -0300

    Add files via upload

---
+++ Etapa1/1_Lasumaanterior/1_Lasumaanterior/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1_Lasumaanterior
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /* Crear 3 variables numéricas con el valor que tu quieras y en otra 
+             variable numérica guardar el valor de la suma de las 3 anteriores.Mostrar por consola.*/
+            int num1 = 24;
+            int num2 = 12;
+            int num3 = 5;
+
+
+        }
+    }
+}
