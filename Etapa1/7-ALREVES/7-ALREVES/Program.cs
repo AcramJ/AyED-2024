8ab884dde39f5348842dfb50aba55bbf1d077605
AcramJ <167466680+AcramJ@users.noreply.github.com>
2024-05-13 21:58:58 -0300

    Add files via upload

---
+++ Etapa1/7-ALREVES/7-ALREVES/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _7_ALREVES
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string letra1, letra2, letra3;
+            Console.WriteLine("Decime 3 letras cualquiera");
+            letra1 = Console.ReadLine();
+            letra2 = Console.ReadLine();
+            letra3 = Console.ReadLine();
+            Console.WriteLine(letra3 + letra2 + letra1);
+            Console.ReadKey(); 
+             
+
+        }
+    }
+}
---
+++ Etapa1/8-EsMayor/8-EsMayor/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _8_EsMayor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int edad;
+            Console.WriteLine("¿Cuantos años tenes?");
+            edad = int.Parse(Console.ReadLine());
+            {
+                if (edad >= 18)
+                {
+                    Console.WriteLine("Sos mayor de edad");
+                }
+                else if (edad < 18)
+                {
+                    Console.WriteLine("Sos menor de edad");
+                }
+                
+
+
+                Console.ReadKey();
+            }
+        }
+    }
+}
+
