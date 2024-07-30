e320c7abe7bd0aa1fcc970f92251c985a5e35d99
AcramJ <167466680+AcramJ@users.noreply.github.com>
2024-07-30 07:38:54 -0300

    Add files via upload

---
+++ Etapa2/13_Marca_ObtenerDiagonalPrincipal/13_Marca_ObtenerDiagonalPrincipal/13_Marca_ObtenerDiagonalPrincipal/Program.cs
@@ -0,0 +1,43 @@
+﻿namespace _13_Marca_ObtenerDiagonalPrincipal
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Ingrese el tamaño de la matriz: ");
+            int n = int.Parse(Console.ReadLine());
+
+            int[,] matriz = new int[n, n];
+            int[] diagonalPrincipal = new int[n];
+            Random random = new Random();
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    matriz[i, j] = random.Next(1, 101);
+                }
+            }
+            for (int i = 0; i < n; i++)
+            {
+                diagonalPrincipal[i] = matriz[i, i];
+            }
+
+            Console.WriteLine("Matriz generada:");
+            for (int fila = 0; fila < n; fila++)
+            {
+                for (int columna = 0; columna < n; columna++)
+                {
+
+                    Console.Write(matriz[fila, columna] + "\t");
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine("\nDiagonal principal:");
+            for (int i = 0; i < n; i++)
+            {
+                Console.Write(diagonalPrincipal[i] + "\t");
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Etapa2/14_Marca_CalificacionesEstudiantiles/14_Marca_CalificacionesEstudiantiles/14_Marca_CalificacionesEstudiantiles/Program.cs
@@ -0,0 +1,39 @@
+﻿namespace _14_Marca_CalificacionesEstudiantiles
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ingrese el número de estudiantes:");
+            int n = int.Parse(Console.ReadLine());
+            object[,] estudiantes = new object[n, 3];
+            for (int i = 0; i < n; i++)
+            {
+                Console.WriteLine($"Ingrese los datos del estudiante {i + 1}:");
+
+                Console.Write("Nombre: ");
+                estudiantes[i, 0] = Console.ReadLine();
+
+                Console.Write("Edad: ");
+                estudiantes[i, 1] = int.Parse(Console.ReadLine());
+
+                Console.Write("Calificación: ");
+                estudiantes[i, 2] = double.Parse(Console.ReadLine());
+            }
+            Console.WriteLine("\nInformación de los estudiantes:\n");
+            Console.WriteLine("Nombre\tEdad\tCalificación");
+            for (int i = 0; i < n; i++)
+            {
+                
+                for (int j = 0; j < 3; j++)
+                {
+                    Console.Write(estudiantes[i, j] + "\t");
+                }
+                Console.WriteLine();
+            }
+            Console.ReadKey();
+    }
+}
+
+}
+
