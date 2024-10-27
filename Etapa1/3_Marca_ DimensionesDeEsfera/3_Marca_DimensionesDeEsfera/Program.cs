57d11b7304cf1ec3a9cde5a93067d2d061c13745
AcramJ <167466680+AcramJ@users.noreply.github.com>
2024-10-27 19:43:10 -0300

    Add files via upload

---
+++ Etapa1/3_Marca_ DimensionesDeEsfera/3_Marca_DimensionesDeEsfera/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _3___DimensionesDeEsfera
+{
+    class Program
+    {
+        static void Main(string[] args)
+
+        {
+            int radio;
+            Console.WriteLine("Dime el radio de la esfera");
+            radio = int.Parse(Console.ReadLine());
+            Console.WriteLine("La supercie es de = " + (4 * Math.PI*(radio * radio)));
+            Console.WriteLine("El volumen es de = " + (4 * Math.PI / 3 * (radio * radio * radio)));
+            Console.ReadKey();
+
+        }
+    }
+}
