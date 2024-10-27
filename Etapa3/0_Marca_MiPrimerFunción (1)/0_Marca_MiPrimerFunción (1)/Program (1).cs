273305ef9be1f8e4b174579ada16e8a565ee1c8a
AcramJ <167466680+AcramJ@users.noreply.github.com>
2024-10-27 20:40:18 -0300

    Add files via upload

---
+++ Etapa3/0_Marca_MiPrimerFunción (1)/0_Marca_MiPrimerFunción (1)/Program (1).cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _0_Marca_MiPrimerFunción
+{
+    class Program
+    {
+        static string y (string x)
+        {
+            x = "Holaa " + x;
+            return x;
+        }
+        static void Main(string[] args)
+
+        {
+            string nombre;
+            Console.WriteLine("Ingrese su nombre:");
+            nombre = Console.ReadLine();
+            Console.WriteLine(y(nombre));
+
+            Console.ReadKey();
+        }
+    }
+}
