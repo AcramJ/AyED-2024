fd9d41f85ba3c2f71a8c7f5e5a662e253320af43
AcramJ <167466680+AcramJ@users.noreply.github.com>
2024-10-27 20:22:43 -0300

    Add files via upload

---
+++ Etapa1/0_Marca_CalculadoraBasica/0_Marca_CalculadoraBasica/0_Marca_CalculadoraBasica/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace _0_Marca_CalculadoraBasica
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Ingrese el primer número: ");
+            int num1 = int.Parse(Console.ReadLine());
+
+            Console.Write("Ingrese el segundo número: ");
+            int num2 = int.Parse(Console.ReadLine());
+
+            int suma = num1 + num2;
+            int resta = num1 - num2;
+            int multiplicacion = num1 * num2;
+            float division = num1 / num2;
+            int resto = num1 % num2;
+
+            Console.WriteLine("Suma: " + suma);
+            Console.WriteLine("Resta: " + resta);
+            Console.WriteLine("Multiplicación: " + multiplicacion);
+            Console.WriteLine("División: " + division);
+            Console.WriteLine("Resto: " + resto);
+
+
+        }
+    }
+}
