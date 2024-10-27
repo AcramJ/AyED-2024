ee80d30c669f4a42123a9cb447e2b1de2d45769b
AcramJ <167466680+AcramJ@users.noreply.github.com>
2024-10-27 19:50:57 -0300

    Add files via upload

---
+++ Etapa1/18_MarcaHastaQueSI/18_Marca_HastaQueSI/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _18___HastaQueSI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string SI;
+            Console.WriteLine("Ingrese SI para terminar el programa");
+            SI = Console.ReadLine();
+            while (SI != "SI")
+            {
+                Console.WriteLine("Desea terminar el programa");
+                SI = Console.ReadLine();
+            }
+            if (SI == "SI") 
+            {
+                Console.WriteLine("¡Hasta la vista!");
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Etapa1/19_Marca_HastaN/19_Marca_HastaN/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _19___HastaN
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num, n;
+            num = 0;
+            Console.WriteLine("Dame un numero");
+            n = int.Parse(Console.ReadLine());
+            while (num <= n) 
+            {
+                if (num % 2 == 0)
+                { 
+                    Console.WriteLine(num);
+                }
+                num++;
+            }
+            Console.ReadKey();
+
+
+
+        }
+    }
+}
